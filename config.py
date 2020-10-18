76f24c63c85560f16a15248b4544ceec0acc1562
ReekyStive <reekystive@gmail.com>
2020-10-19 00:13:33 +0800

    Fixed a data type bug

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 driver_path = '/path/to/chrome/webdriver'
 
-username = 20201234567
-password = 123456
+username = '20201234567'
+password = '123456'
 
 min_temperature = 35.5
 max_temperature = 36.9
